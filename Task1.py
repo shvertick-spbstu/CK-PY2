0e3c0a1c9255d705aa92d61691273da5bed1eb68
shvertick-spbstu <115480529+shvertick-spbstu@users.noreply.github.com>
2023-01-18 18:35:16 +0300

    Update and rename pass.py to Task1.py

---
+++ Task1.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+         "pages": 400,
+    }
+]
+
+class Book(BaseModel):
+    id_: int
+    name: str
+    pages: int
+
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f"Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})"
+
+if __name__ == '__main__':
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)
+    print(list_books)
