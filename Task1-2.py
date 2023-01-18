c477451bf52f5b756caf366afa403f38dc6ab028
shvertick-spbstu <115480529+shvertick-spbstu@users.noreply.github.com>
2023-01-18 18:49:27 +0300

    Update Task1-2.py

--- Task1-2.py
+++ Task1-2.py
@@ -53,19 +53,16 @@ class Library(BaseModel):
         raise ValueError("Книги с запрашиваемым id не существует")
 
 if __name__ == '__main__':
-#Task1
-    list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
-    ]
-    for book in list_books:
-        print(book)
-    print(list_books)
-#Task 2
     empty_library = Library()
     print(empty_library.get_next_book_id())
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
+    print("Task1")
+    for book in list_books:
+        print(book)
+    print(list_books)
+    print("Task2")
     library_with_books = Library(books=list_books)
     print(library_with_books.get_next_book_id())
     print(library_with_books.get_index_by_book_id(1))
